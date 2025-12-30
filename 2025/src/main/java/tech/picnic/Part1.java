e0bee489c7ac593f355f0203fc22b20dc900c536
Jean de Leeuw <jeandeleeuw@gmail.com>
2025-12-30 14:41:48 +0100

    Picnic AoC Solution Part 1

--- 2025/src/main/java/tech/picnic/Part1.java
+++ 2025/src/main/java/tech/picnic/Part1.java
@@ -10,7 +10,6 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class Part1 {
 
@@ -44,7 +43,7 @@ public class Part1 {
 
         private int coldness;
 
-        TemperatureZone(int coldness){
+        TemperatureZone(int coldness) {
             this.coldness = coldness;
         }
 
@@ -53,11 +52,14 @@ public class Part1 {
         }
     }
 
-    public record ToteStack(List<Tote> totes) {}
+    public record ToteStack(List<Tote> totes) {
+    }
 
-    public record ToteBatch(List<Tote> batch) {}
+    public record ToteBatch(List<Tote> batch) {
+    }
 
-    public record AllTotesProcessed(List<ToteStack> stacks) {}
+    public record AllTotesProcessed(List<ToteStack> stacks) {
+    }
 
     public List<ToteBatch> createBatches(List<Tote> upcomingTotes) {
         List<ToteBatch> batches = new ArrayList<>();
@@ -70,6 +72,7 @@ public class Part1 {
             // Tote cannot be stacked on top of the previous tote, so this needs to be a new batch
             if (currentTote.tempZone().isColderThan(previousTote.tempZone()) || currentTote.weight() > previousTote.weight()) {
                 batches.add(new ToteBatch(List.copyOf(currentBatch)));
+                currentBatch = new ArrayList<>();
             }
             currentBatch.add(currentTote);
             previousTote = currentTote;
@@ -86,9 +89,6 @@ public class Part1 {
         List<AllTotesProcessed> processed = new ArrayList<>();
         for (int i = 1; i <= Math.min(4, upcomingTotes.size()); i++) {
             ToteStack newStack = new ToteStack(upcomingTotes.subList(0, i));
-            if (!isValid(newStack)) {
-                continue;
-            }
             List<ToteStack> processedStacksCopy = new ArrayList<>(processedStacks);
             processedStacksCopy.add(newStack);
             processed.addAll(buildAllPossibilities(processedStacksCopy, new ArrayList<>(upcomingTotes.subList(i, upcomingTotes.size()))));
@@ -96,31 +96,13 @@ public class Part1 {
         return processed;
     }
 
-    private boolean isValid(ToteStack stack) {
-        Tote previousTote = stack.totes().getFirst();
-        for (int i = 1; i < stack.totes().size(); i++) {
-            Tote currentTote = stack.totes().get(i);
-            // A heavier tote cannot be stacked on top of a lighter tote
-            if (currentTote.weight() > previousTote.weight()) {
-                return false;
-            }
-
-            // A colder tote cannot be stacked on top of a warmer tote
-            if (currentTote.tempZone().isColderThan(previousTote.tempZone())) {
-                return false;
-            }
-        }
-        return true;
-    }
-
     public long minimalCost(List<Tote> upcomingTotes) {
         long totalCost = 0;
         // Batch the tote list to reduce the number of possible tote stack combinations
         List<ToteBatch> batches = createBatches(upcomingTotes);
         for (ToteBatch batch : batches) {
-            var a = buildAllPossibilities(new ArrayList<>(), batch.batch());
             // Find all possible stack combinations, and use the lowest cost one
-            totalCost += a
+            totalCost += buildAllPossibilities(new ArrayList<>(), batch.batch())
                     .stream()
                     .mapToLong(this::cost)
                     .min()
@@ -141,10 +123,10 @@ public class Part1 {
             case 2 -> 25;
             case 3 -> 10;
             case 4 -> 0;
-            default -> throw new RuntimeException("Stack size can only by 1 - 4, but was: %s, %s".formatted(stack.totes().size(), stack));
+            default ->
+                    throw new RuntimeException("Stack size can only by 1 - 4, but was: %s, %s".formatted(stack.totes().size(), stack));
         };
     }
-
 
 
     public static void main(String[] args) throws IOException {
